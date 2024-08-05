30e49cb8011c47fc0e963dad465205ac43644088
emanuele.di-molfetta@ext.wefox.com <emanuele.di-molfetta@ext.wefox.com>
2024-08-05 17:46:28 +0200

    feat: Serving Static Pages

--- src/app.py
+++ src/app.py
@@ -1,4 +1,9 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
+
+class ServerException(Exception):
+    """Server internal error."""
+    pass
 
 class RequestHandler(BaseHTTPRequestHandler):
     '''Handle HTTP requests by returning a fixed 'page'.'''
@@ -20,9 +25,59 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     # Handle a GET request.
     def do_GET(self):
-        page = self.create_page()
-        self.send_page(page)
+        try:
+            # Figure out what exactly is being requested.
+            full_path = os.getcwd() + self.path
 
+            # It doesn't exist...
+            if not os.path.exists(full_path):
+                raise ServerException("'{0}' not found".format(self.path))
+
+            # ...it's a file...
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+
+            # ...it's something we don't handle.
+            else:
+                raise ServerException("Unknown object '{0}'".format(self.path))
+
+        # Handle errors.
+        except Exception as msg:
+            self.handle_error(msg)
+
+    # Read File method
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
+            self.handle_error(msg)
+    
+    # Error Handling page
+    Error_Page = '''\
+        <html>
+        <body>
+        <h1>Error accessing {path}</h1>
+        <p>{msg}</p>
+        </body>
+        </html>
+        '''            
+
+    # Error Handling method
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content.encode('utf-8'), 404)
+
+    # Send actual content.
+    def send_content(self, content, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(len(content)))
+        self.end_headers()
+        self.wfile.write(content)                
+    
     def send_page(self, page):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
